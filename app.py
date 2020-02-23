2febdfb6d381aacf9ee21fde6deed593f747c914
Clet Simon Sevillejo <cssevillejo@gmail.com>
2020-02-23 16:19:08 +0800

    conver word to lowercase before search

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import json
 data = json.load(open("data.json"))
 
 def translate(word):
-    if word in data:
-        return data[word]
+    if word.lower() in data:
+        return data[word.lower()]
     else:
         return "The word doesn't exist. Please double check it."
 
