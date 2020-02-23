89347a7ae041ceb03f1916bae7c45aac1fd3edec
Clet Simon Sevillejo <cssevillejo@gmail.com>
2020-02-23 16:43:15 +0800

    add option if word input has similar word to the existing data

--- app.py
+++ app.py
@@ -8,7 +8,13 @@ def translate(word):
     if w in data:
         return data[w]
     elif len(get_close_matches(w, data.keys(), cutoff=0.8)) > 0:
-        return "Did you mean '%s' instead?" % get_close_matches(w, data.keys(), cutoff=0.8)[0]
+        yn = input("Did you mean '%s' instead? \nEnter Y if yes, or N if no: " % get_close_matches(w, data.keys(), cutoff=0.8)[0])
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys(), cutoff=0.8)[0]]
+        elif yn == "N":
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your entry."
     else:
         return "The word doesn't exist. Please double check it."
 
