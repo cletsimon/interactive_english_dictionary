56ec81126ae93c954cb86dd5daae25f575b7c6cb
Clet Simon Sevillejo <cssevillejo@gmail.com>
2020-02-23 16:18:17 +0800

    add condition if word exists

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ import json
 data = json.load(open("data.json"))
 
 def translate(word):
-    return data[word]
+    if word in data:
+        return data[word]
+    else:
+        return "The word doesn't exist. Please double check it."
 
 
 input_word = input("Enter word:")
