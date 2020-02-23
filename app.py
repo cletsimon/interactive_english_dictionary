539b0925cbf32db3b493de322c298c539b165da0
Clet Simon Sevillejo <cssevillejo@gmail.com>
2020-02-23 16:47:39 +0800

    cleaner display

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ data = json.load(open("data.json"))
 def translate(word):
     w = word.lower()
     if w in data:
-        return data[w]
+        return "\n".join(data[w])
     elif len(get_close_matches(w, data.keys(), cutoff=0.8)) > 0:
         yn = input("Did you mean '%s' instead? \nEnter Y if yes, or N if no: " % get_close_matches(w, data.keys(), cutoff=0.8)[0])
         if yn == "Y":
-            return data[get_close_matches(w, data.keys(), cutoff=0.8)[0]]
+            return "\n".join(data[get_close_matches(w, data.keys(), cutoff=0.8)[0]])
         elif yn == "N":
             return "The word doesn't exist. Please double check it."
         else:
@@ -20,4 +20,5 @@ def translate(word):
 
 
 input_word = input("Enter word:")
-print(translate(input_word))
+output = translate(input_word)
+print(output)
